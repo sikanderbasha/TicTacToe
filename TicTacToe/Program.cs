b7c0703f2014dfdff97a507bb69d6feb3cc8c5ea
Sikander <sikku.rk405@gmail.com>
2022-02-10 10:09:47 +0530

    [Sikander] Added create board program

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Createboard create = new Createboard();
+            create.Create();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/Createboard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Createboard
+    {
+        public void Create()
+        {
+            char[] board = new char[10];       
+            for (int StartingIndex = 0; StartingIndex < board.Length; StartingIndex++) 
+                Console.WriteLine(board[StartingIndex] = ' '); 
+        }
+    }
+}
